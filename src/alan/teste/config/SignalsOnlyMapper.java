0b8a81b64c526f3378a68cce7295dadcebfe92d5
habea <habea@DESKTOP-S97ANH0>
2016-06-14 18:46:26 -0400

    Committing

---
+++ src/alan/teste/config/SignalsOnlyMapper.java
@@ -0,0 +1,19 @@
+package alan.teste.config;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.jmlspecs.ajmlrac.runtime.JMLExceptionalPostconditionError;
+
+import alan.teste.entities.Message;
+
+public class SignalsOnlyMapper implements ExceptionMapper<JMLExceptionalPostconditionError> {
+
+	@Override
+	public Response toResponse(JMLExceptionalPostconditionError ex) {
+		Message message = new Message("500", "location", ex.getMessage());
+
+		return Response.status(500).entity(message).build();
+	}
+
+}
